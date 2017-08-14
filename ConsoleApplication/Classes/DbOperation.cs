3cc5a198e72daeccae2f2d1e89c28fa1561f6955
EMAD GAMEL ANTON <emad.gamil126@el-eng.menofia.edu.eg>
2017-08-14 13:15:40 +0200

    More Dynamic

---
+++ ConsoleApplication/Classes/DbOperation.cs
@@ -0,0 +1,109 @@
+﻿using ConsoleApplication.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication.Classes
+{
+    class DbOperation
+    {
+        public void ViewRecords()
+        {
+            var db = new pharmacydbEntities();
+
+            //Show recordes
+            try
+            {
+                foreach (var customer in db.customers.ToList())
+                {
+                    Console.WriteLine("CustomerInfo - {0}-{1}", customer.cust_name, customer.cust_address);
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.ToString());
+
+            }
+        }
+
+        public string InsertRecord(string custName,string custAddress)
+        {
+            var db = new pharmacydbEntities();
+
+            // Insert  
+            try
+            {
+                var cust = GetCustomers(custName,custAddress);
+                db.customers.AddRange(cust);
+                db.SaveChanges();
+                Console.WriteLine("Info saved");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.ToString());
+            }
+            return custName;
+        }
+
+        public string UpdateRecord(string custName,string newName)
+        {
+            var db = new pharmacydbEntities();
+
+            //Update
+            try
+            {
+                customer cu = db.customers.First(c => c.cust_name == custName);
+                {
+                    cu.cust_name = newName;
+                };
+                db.SaveChanges();
+                Console.WriteLine("Info was Updated");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.ToString());
+            }
+            return custName;
+        }
+
+        public string DeleteRecord(string custName)
+        {
+            var db = new pharmacydbEntities();
+            //Delete
+            try
+            {
+                var del = (from c in db.customers
+                           where c.cust_name == custName
+                           select c).First();
+
+                db.customers.Remove(del);
+                db.SaveChanges();
+                Console.WriteLine("Customer Info Deleted");
+
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.ToString());
+            }
+            return custName;
+
+        }
+
+        public static List<customer> GetCustomers(string custName,string custAddress)
+        {
+            var customers = new List<customer>
+                    {
+                        new customer()
+                            {
+                                cust_name    = custName,
+                                cust_address = custAddress
+                            }
+            };
+            return customers;
+        }
+
+    }
+}
+
--- ConsoleApplication/Program.cs
+++ ConsoleApplication/Program.cs
@@ -11,10 +11,9 @@ namespace ConsoleApplication
     {
         static void Main(string[] args)
         {
+            Classes.DbOperation dbObject = new Classes.DbOperation();
             for (;;)
-            {
-                var db = new pharmacydbEntities();
-
+            {         
                 Console.WriteLine();
                 Console.WriteLine("Please Select Operation");
                 Console.WriteLine("[1] Insert , [2] Update , [3] Delete , [4] Show Data");
@@ -24,88 +23,37 @@ namespace ConsoleApplication
                 // there are 4 choices 
                 if (x.ToString() == "1")
                 {
-                    // Insert  
-                    try
-                    {
-                        var cust = GetCustomers();
-                        db.customers.AddRange(cust);
-                        db.SaveChanges();
-                        Console.WriteLine("Info saved");
-                    }
-                    catch(Exception e)
-                    {
-                        Console.WriteLine(e.ToString());
-                    }
+                    Console.Write("Enter Customer Name : ");
+                    var custName = Console.ReadLine();
+                    Console.Write("Enter Customer Address : ");
+                    var custAddress = Console.ReadLine();
+
+                    dbObject.InsertRecord(custName.ToString(), custAddress.ToString());
                 }
 
                 if (x.ToString() == "2")
                 {
-                    //Update
-                    try
-                    {
-                        customer cu = db.customers.First(c => c.cust_name == "John");
-                        {
-                            cu.cust_name = "Emad";
-                        };
-                        db.SaveChanges();
-                        Console.WriteLine("Info was Updated");
-                    }
-                    catch (Exception e)
-                    {
-                        Console.WriteLine(e.ToString());
-                    }
+                    Console.Write("Enter Customer Name : ");
+                    var custName = Console.ReadLine();
+                    Console.Write("Enter Update Name : ");
+                    var newName = Console.ReadLine();
+
+                    dbObject.UpdateRecord(custName.ToString(), newName.ToString());
                 }
                 if (x.ToString() == "3")
                 {
-                    //Delete
-                    try
-                    {
-                        var del = (from c in db.customers
-                                   where c.cust_name == "Krkr"
-                                   select c).First();
-
-                        db.customers.Remove(del);
-                        db.SaveChanges();
-                        Console.WriteLine("Customer Info Deleted");
-
-                    }
-                    catch (Exception e)
-                    {
-                        Console.WriteLine(e.ToString());
-                    }
-
+                    Console.Write("Enter Customer Name :- ");
+                    var custName = Console.ReadLine();
+                    
+                    dbObject.DeleteRecord(custName.ToString());
                 }
 
                 if (x.ToString() == "4")
                 {
-                    //Show recordes
-                    try
-                    {
-                        foreach (var customer in db.customers.ToList())
-                        {
-                            Console.WriteLine("CustomerInfo - {0}-{1}", customer.cust_name, customer.cust_address);
-                        }
-                    }
-                    catch (Exception e)
-                    {
-                        Console.WriteLine(e.ToString());
-
-                    }
+                    dbObject.ViewRecords();
                 }
 
             }
-        }
-        public static List<customer> GetCustomers()
-        {
-            var customers = new List<Model.customer>
-                    {
-                        new customer()
-                            {
-                                cust_name    = "Krkr",
-                                cust_address = "Krkr@admin.com"
-                            }
-            };
-            return customers;
         }
     }
 }
